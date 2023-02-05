73048f26151a856a9941070a55d01700c65244ec
Mockingbird404 <alwaysindiscipline@gmail.com>
2023-02-05 07:12:56 -0500

    test

---
+++ brainfuck.py
@@ -0,0 +1,19 @@
+import sys
+
+cell_current : int  = 0
+cell_pointer : int  = 0
+cells		      : list = [0]
+code             : str  = """       ++++++++++++++++++++++++++++ 
+                           		   	   ++++++++++++++++++++++++++++
+                           		   	   ++++++++++++++++++++++++++++
+                           				  +++++++++++++."""
+                           				  
+def interpreter():
+	
+	while cell_pointer <= {0}:
+		
+		i
+	
+if __name__ == "__main__":
+	
+	interpreter()
